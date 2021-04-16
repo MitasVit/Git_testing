7a96c4ccd70aa6f6b8bbfb0e18828b3c945a7dfc
MitasVit <71701503+MitasVit@users.noreply.github.com>
2021-04-16 15:54:49 +0200

    Create Git testing lib.h

---
+++ Git testing lib.h
@@ -0,0 +1,17 @@
+#ifndef __GIT_TESTING_LIB_H__
+#define __GIT_TESTING_LIB_H__
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+namespace Git_testing{
+  void SomeFoo(){
+    cout << endl << "SomeFoo" << endl;
+  }
+
+}
+
+
+#endfi //__GIT_TESTING_LIB_H__
